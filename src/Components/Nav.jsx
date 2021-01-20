ee89bfffa3d93aebbe0ee1fa8cf4b9956de3578a
jcanete <joachimcanete@gmail.com>
2021-01-19 18:29:17 -0600

    individual pokemon cards render information specifc to card type

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 
 export default function Nav () {
+    const [searchState, setSearchState] = useState('');
+
+    const searchChange = (event) => {
+        setSearchState({ ...searchState, [event.target.value]: event.target.name});
+    }
+    const searchSubmit = (event) => {
+        event.preventDefault()
+        setSearchState({ ...searchState, [event.target.name]: event.target.value});
+    }
+
     return (
         <div>
             <nav>
                 <Link to='/' className='homeButt'>Home</Link>
             </nav>
+            <input
+                type='text'
+                name='searchState'
+                className='searchBar'
+                id='search'
+                onChange={searchChange}
+                value={searchSubmit.value}
+            />
         </div>
     )
 }
--- src/Components/PokeDetails.jsx
+++ src/Components/PokeDetails.jsx
@@ -9,22 +9,62 @@ export default function PokeDetails ({ match }) {
         await fetch(`https://api.pokemontcg.io/v1/cards/${match.params.id}`)
         .then(res => res.json())
         .then(res => {
+            console.log(res)
             setPoke(res)
         })
         } catch {
-            console.log(console.error);
         }
     }
     useEffect(() => {
         fectchPoke()
     })
-
-    return (
-        poke && (
+    
+    if (poke && poke.card.supertype === "Pokémon") {
+        return (
+            poke && (
             <div>
-                <h3>{match.params.id}</h3>
                 <img src={poke.card.imageUrl} alt={poke.card.name}/>
+                <h3>{poke.card.name}</h3>
+                <h3>{poke.card.supertype}</h3>
+                <h3>HP = {poke.card.hp}</h3>
+                <h3>{poke && poke.card.attacks[0].name}</h3>
+                <h4>{poke && poke.card.attacks[0].text}</h4>
+                <h3>{poke && poke.card.attacks[1].name}</h3>
+                <h4>{poke && poke.card.attacks[1].text}</h4>
             </div>
+            )
         )
-    )
+    } else if (poke && poke.card.supertype === "Trainer") {
+        return (
+            poke && (
+            <div>
+                <img src={poke.card.imageUrl} alt={poke.card.name}/>
+                <h3>Trainer Type : {poke.card.name}, {poke.card.rarity}</h3>
+                <p>Action: {poke.card.text[0].text}</p>
+            </div>
+            )
+        )
+    } else if (poke && poke.card.supertype === "Energy") {
+        return (
+            poke && (
+            <div>
+                <img src={poke.card.imageUrl} alt={poke.card.name}/>
+                <h3>Energy Type : {poke.card.name}</h3>
+            </div>
+            )
+        )
+    } else {
+        return (
+            <h1>Card Not Found</h1>
+        )
+    }
+
 }
+/*
+CONDITIONAL RENDERING
+set contants for 3 different view states of each superType:
+-Pokemon
+-Trainer
+-Energy
+build tenerary operator to conditionally render based on superType
+*/
